4cc66891ea009558cc5497befc5c2c6be26bce58
TASOS <tasosanas2002@gmail.com>
2026-01-15 18:19:43 +0200

    n

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+for (let i = 0; i < DUMMY_USERS.length; i++) {
+  
+}const randomUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+selectedUser = DUMMY_USERS[randomUserIndex];
 }
