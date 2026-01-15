9ae295d599ff02188dc8d66fd12419b291293da5
TASOS <tasosanas2002@gmail.com>
2026-01-15 17:53:05 +0200

    Creating user component

--- code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [UserComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
