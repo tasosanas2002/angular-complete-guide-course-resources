797d8b104c1a491606fadf084f3ef8df23dda680
TASOS <tasosanas2002@gmail.com>
2026-01-20 10:03:05 +0200

    .

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -10,5 +10,15 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({required: true}) name!: string;
+  @Input({required: true}) userId!: string;
+tasks = [
+  { id: 't1', userId: 'u1', title: 'Finish the Angular course', summary: 'Complete all modules', dueDate: '2026-02-28' },
+  { id: 't2', userId: 'u2', title: 'Go for a walk', summary: 'Morning exercise', dueDate: '2026-01-25' },
+  { id: 't3', userId: 'u3', title: 'Read a book', summary: 'Finish current book', dueDate: '2026-02-15' },
+];
+
+get SelectedUserTasks() {
+  return this.tasks.filter(task => task.userId === this.userId);
+}
 }
